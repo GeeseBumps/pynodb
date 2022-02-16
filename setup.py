cf70db4efd8447f6f33446854628010a9bba0185
GeeseBumps <pgb1227@kaist.ac.kr>
2022-02-16 17:58:01 +0900

    Version Upgrade and Deploy 0.2.0 version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pynodb",
-    version="0.1",
+    version="0.2",
     url="https://github.com/GeeseBumps/pynodb",
     license="MIT",
     author="GeeseBumps",
